d448fafdf5b49106a74d35c6c44e6dc75d5fb3ad
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-17 18:05:20 -0500

    removed bootcamp, backend service data binding, extract api from backend

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,40 +1,53 @@
 'use strict';
 
-angular.module('myApp.notes', ['ngRoute'])
+var noteApp = angular.module('myApp.notes', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+noteApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/notes', {
     templateUrl: 'notes/notes.html',
     controller: 'NotesController'
   });
 }])
 
-.controller('NotesController', function($scope, $http) {
-  var apiBasePath =  'https://elevennote-nov-2014.herokuapp.com/api/v1/';
-  var postNotePath = apiBasePath +  'notes';
-  var apiKey = '$2a$10$zO5VPjSSrfJn3tXjk873VubX/ckdlj5HQC7/yTZ/WF.6dwjzehSd.';
-  var notes = [];
+noteApp.service('NotesBackend', function($http) {
+    var apiBasePath =  'https://elevennote-nov-2014.herokuapp.com/api/v1/';
+    var postNotePath = apiBasePath +  'notes';
+    var apiKey = '$2a$10$zO5VPjSSrfJn3tXjk873VubX/ckdlj5HQC7/yTZ/WF.6dwjzehSd.';
+    var notes = [];
 
-    $http.get(apiBasePath + 'notes.json?api_key=' + apiKey).success(function(noteData){
-      $scope.notes = noteData;
-    });
+    this.getNotes = function () {
+      return notes;
+    };
+
+    this.fetchNotes = function () {
+      $http.get(apiBasePath + 'notes.json?api_key=' + apiKey).success(function(noteData){
+        notes = noteData;
+      });
+
+    };
+    this.postNote = function(note) {
+
+          $http.post(postNotePath, {
+            api_key: apiKey,
+            note:{
+              title: note.title,
+              body_html: note.body_html
+            }
+          }).success(function(noteData){
+             notes = [noteData].concat(notes);
+        });
+    }
+});
+
+noteApp.controller('NotesController', function($scope, $http, NotesBackend) {
+   NotesBackend.fetchNotes();
+
+   $scope.notes = function(){
+     return NotesBackend.getNotes();
+   };
 
   $scope.commit = function(){
-
-    console.log('it is commited');
-    $http.post(postNotePath, {
-      api_key: apiKey,
-      note:{
-        title: 'Cool Title',
-        body_html: 'Less Cool Body'
-      }
-    }).success(function(successData){
-    console.log('Success');
-    console.log(successData)
-  }).error(function(errorData){
-      console.log('error');
-      console.log(errorData);
-    });
+    NotesBackend.postNote($scope.note);
   };
 
 
