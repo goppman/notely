9091c1a853e9b82a8d9453b4d24a40b51a462b31
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-17 16:51:27 -0500

    add list to side bar from http get and post

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -9,6 +9,33 @@ angular.module('myApp.notes', ['ngRoute'])
   });
 }])
 
-.controller('NotesController', [function() {
+.controller('NotesController', function($scope, $http) {
+  var apiBasePath =  'https://elevennote-nov-2014.herokuapp.com/api/v1/';
+  var postNotePath = apiBasePath +  'notes';
+  var apiKey = '$2a$10$zO5VPjSSrfJn3tXjk873VubX/ckdlj5HQC7/yTZ/WF.6dwjzehSd.';
+  var notes = [];
 
-}]);
+    $http.get(apiBasePath + 'notes.json?api_key=' + apiKey).success(function(noteData){
+      $scope.notes = noteData;
+    });
+
+  $scope.commit = function(){
+
+    console.log('it is commited');
+    $http.post(postNotePath, {
+      api_key: apiKey,
+      note:{
+        title: 'Cool Title',
+        body_html: 'Less Cool Body'
+      }
+    }).success(function(successData){
+    console.log('Success');
+    console.log(successData)
+  }).error(function(errorData){
+      console.log('error');
+      console.log(errorData);
+    });
+  };
+
+
+});
