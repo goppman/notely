c09ebc12a4a1b0b6ab8cbbbf3582fb77aab45c74
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-18 15:43:04 -0500

    added rich text editor

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
   'ngRoute',
+  'textAngular',
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-  'myApp.notes'
+  'myApp.notes',
+
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/notes'});
